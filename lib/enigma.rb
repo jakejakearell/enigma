1d7e1fe73d7b66a51b2cc0788308e4fafc854c46
Jacob Arellano <jmm.arellano@gmail.com>
2021-01-14 16:32:38 -0700

    Write code to pass test for date formatter

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -18,8 +18,12 @@ class Enigma
     key
   end
 
-  def date_checker(date)
-    new_date = Time.parse(date.to_s)
+  def date_formatter(arguement)
+    formatted_date = ''
+    new_date = Date._strptime(arguement.to_s, '%d-%m-%Y')
+    formatted_date += new_date[:mday].to_s.rjust 2 ,"0"
+    formatted_date += new_date[:mon].to_s.rjust 2 ,"0"
+    formatted_date += new_date[:year].to_s
   end
 
 end
