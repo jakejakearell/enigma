230a6201d0d9e39e737307429de26ec8100e1e51
Jacob Arellano <jmm.arellano@gmail.com>
2021-01-14 15:21:07 -0700

    Write test to for method to convert date to string

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -1,13 +1,19 @@
+require 'time'
 require_relative './test_helper'
+
 require './lib/enigma'
 
 class EnigmaTest < Minitest::Test
   def setup
-    @engima = Enigma.new
+    @enigma = Enigma.new
   end
 
   def test_it_has_exists
-    assert_instance_of Enigma, @engima
+    assert_instance_of Enigma, @enigma
+  end
+
+  def test_returns_today_as_useable_date
+    assert_equal "011421", @enigma.todays_date
   end
 
 end
