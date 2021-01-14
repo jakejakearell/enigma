ee9256b49706085bad98a28350d8ab0d2b28d828
Jacob Arellano <jmm.arellano@gmail.com>
2021-01-14 15:40:35 -0700

    Fix assertion to accurately check class

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -8,5 +8,14 @@ class Enigma
   def todays_date
     Time.new.strftime("%m/%d/%y").delete("/")
   end
-  
+
+  def key_generator
+    key = ''
+    characters = ("0".."9").to_a
+    5.times do
+      key += characters.sample
+    end
+    key
+  end
+
 end
