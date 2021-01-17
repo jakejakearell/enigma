c365d70e66f8f53c08850ea9195c81f0215c8e8d
Jacob Arellano <jmm.arellano@gmail.com>
2021-01-17 12:02:49 -0700

    Refactor runner files to meet program specifcations

---
+++ decrypt.rb
@@ -0,0 +1,12 @@
+require_relative './lib/enigma'
+
+ARGV
+encrypted_message_file = File.open("files/#{ARGV[0]}", "r" )
+destination_file = File.open("files/#{ARGV[1]}", "w" )
+message = encrypted_message_file.read
+
+enigma = Enigma.new
+decrypted_message = enigma.decrypt("#{message}", "#{ARGV[2]}", "#{ARGV[3]}")
+destination_file.write("#{decrypted_message}")
+
+puts "Created '#{ARGV[1]}' with the key #{ARGV[3]} and date #{ARGV[2]}"
--- encrypt.rb
+++ encrypt.rb
@@ -1,19 +1,12 @@
 require_relative './lib/enigma'
 
-input = ARGV
-plain_message_path = File.open("files/#{input[0]}")
-encrypted_message_file = File.open("files/#{input[1]}", "w" )
+ARGV
+plain_message_path = File.open("files/#{ARGV[0]}")
+encrypted_message_file = File.open("files/#{ARGV[1]}", "w" )
+message = plain_message_path.read.chomp
 
 enigma = Enigma.new
+info = enigma.encrypt("#{message}")
+encrypted_message_file.write("#{info[:encryption]}")
 
-message = plain_message_path.read.chomp
-enigma.encrypt("#{message}", "17/01/21", "11111")
-
-p new_message = enigma.encrypt("#{message}", "17/01/21", "11111")[:encryption]
-
-encrypted_message_file.write("#{new_message}")
-encrypted_message_file.close
-encrypted_message_file = File.open("files/#{input[1]}", "r" )
-new_thing = encrypted_message_file.read
-
-p enigma.decrypt("#{new_thing}", "170121", "11111")
+puts "Created '#{ARGV[1]}' with the key #{info[:key]} and date #{info[:date]}"
