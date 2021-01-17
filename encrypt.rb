0aa71ffc85f208df12ce852c6fc0102993c38a3a
Jacob Arellano <jmm.arellano@gmail.com>
2021-01-17 11:16:24 -0700

    Built out the basics of how a runner will read from files

--- encrypt.rb
+++ encrypt.rb
@@ -1,15 +1,19 @@
 require_relative './lib/enigma'
 
-
 input = ARGV
 plain_message_path = File.open("files/#{input[0]}")
-p message = plain_message_path.read.chomp
-path_to_encrypted_message = File.open("files/#{input[1]}")
-# require "pry"; binding.pry
-# 0
+encrypted_message_file = File.open("files/#{input[1]}", "w" )
 
 enigma = Enigma.new
-# message = "Hi how are you?"
+
+message = plain_message_path.read.chomp
 enigma.encrypt("#{message}", "17/01/21", "11111")
-new_message = enigma.encrypt("#{message}", "17/01/21", "11111")[:encryption]
-p enigma.decrypt("#{new_message}", "170121", "11111")
+
+p new_message = enigma.encrypt("#{message}", "17/01/21", "11111")[:encryption]
+
+encrypted_message_file.write("#{new_message}")
+encrypted_message_file.close
+encrypted_message_file = File.open("files/#{input[1]}", "r" )
+new_thing = encrypted_message_file.read
+
+p enigma.decrypt("#{new_thing}", "170121", "11111")
