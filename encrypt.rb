b36bed9ba506baa2a4f0de701a94cbd06e433bc6
Jacob Arellano <jmm.arellano@gmail.com>
2021-01-17 10:29:14 -0700

    Add a way to load a txt file into encryption method

--- encrypt.rb
+++ encrypt.rb
@@ -2,6 +2,14 @@ require_relative './lib/enigma'
 
 
 input = ARGV
-enigma = Enigma.new
+plain_message_path = File.open("files/#{input[0]}")
+p message = plain_message_path.read.chomp
+path_to_encrypted_message = File.open("files/#{input[1]}")
+# require "pry"; binding.pry
+# 0
 
-p enigma.encrypt("#{input[0]}")
+enigma = Enigma.new
+# message = "Hi how are you?"
+enigma.encrypt("#{message}", "17/01/21", "11111")
+new_message = enigma.encrypt("#{message}", "17/01/21", "11111")[:encryption]
+p enigma.decrypt("#{new_message}", "170121", "11111")
