53488e646153a5b072d2ee119a6866fc5ebe87df
Jacob Arellano <58375638+jakejakearell@users.noreply.github.com>
2021-01-19 07:33:51 -0700

    Update crack_runner.rb
    remove pry

--- crack_runner.rb
+++ crack_runner.rb
@@ -3,7 +3,7 @@ require_relative './lib/cracking'
 ARGV
 enigma = Cracking.new
 info = enigma.crack(ARGV[0], ARGV[2])
-require "pry"; binding.pry
+
 destination_file = File.open("files/#{ARGV[1]}", "w" )
 destination_file.write("#{info[:decryption]}")
 
