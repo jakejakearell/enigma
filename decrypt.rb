a4f4ebc0ec56692ade3cbbd44c304b1e348b6708
Jacob Arellano <jmm.arellano@gmail.com>
2021-01-18 17:31:29 -0700

    Refactor runner files for downstream refactors, verify encryption working on txt files

--- decrypt.rb
+++ decrypt.rb
@@ -1,11 +1,10 @@
 require_relative './lib/decrypt'
 
 ARGV
-encrypted_message_file = File.open("files/#{ARGV[0]}", "r" )
-destination_file = File.open("files/#{ARGV[1]}", "w" )
-message = encrypted_message_file.read
-
 enigma = Decrypt.new
-decrypted_message = enigma.decrypt("#{message}", "#{ARGV[2]}", "#{ARGV[3]}")
-destination_file.write("#{decrypted_message}")
-puts "Created '#{ARGV[1]}' with the key #{ARGV[3]} and date #{ARGV[2]}"
+info = enigma.decrypt(ARGV[0], ARGV[2], ARGV[3])
+
+destination_file = File.open("files/#{ARGV[1]}", "w" )
+destination_file.write("#{info[:decryption]}")
+
+puts "Created '#{ARGV[1]}' with the key #{ARGV[2]} and date #{ARGV[3]}"
