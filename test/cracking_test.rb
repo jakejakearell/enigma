01577219aa539c9682a4f187970c72a5c2bae49a
Jacob Arellano <jmm.arellano@gmail.com>
2021-01-18 20:02:01 -0700

    Fix assertions and remove repeat test

--- test/cracking_test.rb
+++ test/cracking_test.rb
@@ -12,13 +12,15 @@ class CrackingTest < Minitest::Test
 
   def test_it_find_ordinal_values
     assert_equal [7, 18, 18, 8], @cracking.ordinal_values("vjqtbeaweqihssi")
-  end
-
-  def test_expected_ordinal_values_of_orginal_message
-    assert_equal [26, 4, 13, 3]
+    assert_equal [26, 4, 13, 3], @cracking.ordinal_values(" end")
   end
 
   def test_it_can_figure_out_the_location_of_shifts
-    assert_equal "hello world end", @cracking.shift_location("vjqtbeaweqihssi")
+    assert_equal 18, @cracking.shift_location("vjqtbeaweqihssi")[:a]
+    assert_equal 18, @cracking.shift_location("vjqtbeaweqihssi")[:b]
+    assert_equal 8, @cracking.shift_location("vjqtbeaweqihssi")[:c]
+    assert_equal 7, @cracking.shift_location("vjqtbeaweqihssi")[:d]
+    assert_nil @cracking.shift_location("vjqtbeaweqihssi")[:e]
+
   end
 end
