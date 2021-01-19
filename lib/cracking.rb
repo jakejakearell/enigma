fa967296d3080a18054c1b27d439144a613d45a5
Jacob Arellano <jmm.arellano@gmail.com>
2021-01-18 21:19:49 -0700

    Close end spaces and remove useless variable assignment

--- lib/cracking.rb
+++ lib/cracking.rb
@@ -4,7 +4,7 @@ class Cracking < Decrypt
 
     def ordinal_values(message)
       end_characters = message[-4..-1].split("")
-      ordinal = end_characters.map do |character|
+      end_characters.map do |character|
         if character.ord == 32
             26
         else
@@ -17,13 +17,13 @@ class Cracking < Decrypt
       ordinals = ordinal_values(message)
       characters_assinger = message.length % 4
       if characters_assinger % 4 == 0
-          { :a => (1+ordinals[0]), :b => (4-ordinals[1]), :c => (13-ordinals[2]), :d => (3-ordinals[3])}
+        {:a => (1+ordinals[0]), :b => (4-ordinals[1]), :c => (13-ordinals[2]), :d => (3-ordinals[3])}
       elsif characters_assinger % 4 == 1
-         { :a => (3-ordinals[3]), :b => (1+ordinals[0]), :c => (4-ordinals[1]), :d => (13-ordinals[2])}
+        {:a => (3-ordinals[3]), :b => (1+ordinals[0]), :c => (4-ordinals[1]), :d => (13-ordinals[2])}
       elsif characters_assinger % 4 == 2
-         {:a => (13-ordinals[2]), :b => (3-ordinals[3]), :c => (1+ordinals[0]), :d => (4-ordinals[1])}
+        {:a => (13-ordinals[2]), :b => (3-ordinals[3]), :c => (1+ordinals[0]), :d => (4-ordinals[1])}
       else characters_assinger % 4 == 3
-         {:a => (4-ordinals[1]), :b => (13-ordinals[2]), :c => (3-ordinals[3]), :d => (1+ordinals[0])}
+        {:a => (4-ordinals[1]), :b => (13-ordinals[2]), :c => (3-ordinals[3]), :d => (1+ordinals[0])}
       end
     end
 
@@ -36,5 +36,4 @@ class Cracking < Decrypt
       end
       new_message
     end
-
 end
